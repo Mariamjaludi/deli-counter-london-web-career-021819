ebb72c6d13cad0a666fcabebf8b940889daf763d
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 16:48:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,15 @@
 # Write your code here.
-
+katz_deli = []
 =begin the line method that shows everyone their current place
 in the line. If there is nobody in line, it should say "The line
 is currently empty.".
 =end
-def line(array)
+def line(katz_deli)
   if array.length == 0
     puts "The line is currently empty."
+  end
+  katz_deli.each_with_index | x, i| do puts "#{x} is #{i + 1} in line."; end
+end
+
+def take_a_number
+
