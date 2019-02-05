bcc457d3f51cbcf8adbd851355a59191a984d463
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 16:44:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+
+=begin the line method that shows everyone their current place 
+in the line. If there is nobody in line, it should say "The line 
+is currently empty.".
+=end
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  
