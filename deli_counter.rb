b396aa7700554f27a36c688249b1597369de7825
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 16:58:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,6 @@
 # Write your code here.
 katz_deli = []
-=begin the line method that shows everyone their current place
-in the line. If there is nobody in line, it should say "The line
-is currently empty.".
-=end
+
 def line(katz_deli)
   if array.length == 0
     puts "The line is currently empty."
