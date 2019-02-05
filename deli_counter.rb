82e97766ebed4844a38f49bb6868d00c4b950f6d
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 17:08:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   end
-  print "The line is currently: "
-  katz_deli.each_with_index do | x, i| print "#{i + 1}. #{x} "; end
+  string = "The line is currently: "
+  katz_deli.each_with_index do | x, i| string += "#{i + 1}. #{x} "; end
+  puts string
 end
 
 def take_a_number(katz_deli, new_customer)
